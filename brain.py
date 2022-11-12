396e44ac63cc2247b5356635f58e5c2c843ecec9
Igor-Laptop <Igordecc@gmail.com>
2022-11-13 01:25:17 +0400

    Add simple mood and memory

--- brain.py
+++ brain.py
@@ -1,23 +1,36 @@
 # AC - artificial consiousness
 
 import msvcrt
-_sum = ""
+import time
+import sys
+
+class Memory:
+    def __init__(self) -> None:
+        self._memory = []  # the list of events. Simple liniar model of memory. sdf
+        self._sum = ""
+
+
 # main loop
 # only inside loop AC can exist
 def nastroenie():
     print("I'm born")
-    _sum = ""
-    while True:
-        if msvcrt.kbhit():
-            while not msvcrt.getch().hex()=="0d":
-                print(msvcrt.getch().hex())
-                # _char = msvcrt.getwch()
-                # _sum.join(_char)
-                input("")
-        print(_sum)
-        if _sum=="die":
+    mem = Memory()
+    while True:   # Main live loop
+         # --- function that saves input to then replay it ---
+        if msvcrt.kbhit(): 
+            key = msvcrt.getch()
+            while not key.hex()=="0d":      # press enter to same the phrase
+                print(key.decode(), end='', flush=True)  # print each entered key
+                mem._sum += key.decode()
+                key = msvcrt.getch()
+        print(mem._sum)
+        # ----
+        # time.sleep(1)
+        if mem._sum=="die":
             break
 
 
 if __name__=="__main__":
+
     nastroenie()
+    # print(key, end="")
